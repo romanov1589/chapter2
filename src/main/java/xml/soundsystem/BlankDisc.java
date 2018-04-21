d8047f250c13db590ac8d633c75b458b3fc91daa
romanov1589 <35072304+romanov1589@users.noreply.github.com>
2018-04-21 21:06:46 +0200

    Changed to XML configuration.Add values

---
+++ src/main/java/xml/soundsystem/BlankDisc.java
@@ -0,0 +1,15 @@
+package xml.soundsystem;
+
+public class BlankDisc implements CompactDisc {
+    private String title;
+    private String artist;
+
+    public BlankDisc(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public void play() {
+        System.out.println("Odtwarzam utwór " + title + " autorstwa " + artist);
+    }
+}
